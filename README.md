96d0b3c574f26d757dbb67903d2e2033e121f494
Bryan Park <bryan-j-park@outlook.com>
2022-10-16 13:26:51 -1000

    created a game of catching pokemon

---
+++ README.md
@@ -0,0 +1,11 @@
+this is  a project I created in order to practice using HTML, CSS and JavaScript.
+
+The main focus of this project was to start using basic functions in JavaScript in order to have it change elements on the webpage. 
+
+I used CSS to make the layout and change the cursor to a pokeball in order to anime the catches a little bit more. 
+
+Functions like querySelector were used in order to select the individual buttons to animate the page. 
+
+the pokemon caught counter in the top right corner was created by using JavaScript in order to calculate the amount of pokemon that.
+
+Please enjoy my little pokemon game, gotta catch'em all!
---
+++ script.js
@@ -0,0 +1,101 @@
+// catching Pokemon!! // 
+var caught = 0;
+var numberCaught = document.querySelector('#pokemonCaughtBox');
+
+var magikarp = document.querySelector('#magikarp');
+function catchMagikarp(){
+  magikarp.remove();
+  alert('You Caught Magikarp!');
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var eevee = document.querySelector('#eevee');
+function catchEevee(){
+  eevee.remove();
+  alert('You Caught Eevee!')
+  caught++; 
+  numberCaught.innerText = caught;
+}
+
+var squirtle = document.querySelector('#squirtle')
+function catchSquirtle(){
+  squirtle.remove();
+  alert('You Caught Squirtle!')
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var snorlax = document.querySelector('#snorlax')
+function catchSnorlax(){
+  snorlax.remove();
+  alert('You Caught Snorlax!')
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var tentacool = document.querySelector('#tentacool');
+function catchTentacool(){
+  tentacool.remove();
+  alert('You Caught Tentacool!')
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var krabby = document.querySelector('#krabby');
+function catchKrabby(){
+  krabby.remove();
+  alert('You Caught Krabby!');
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var gyarados = document.querySelector('#gyarados');
+function catchGyarados(){
+  gyarados.remove();
+  alert('You Caught Gyarados!')
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var geodude = document.querySelector('#geodude');
+function catchGeodude(){
+  geodude.remove();
+  alert('You Caught Geodude!');
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var onyx = document.querySelector('#onyx');
+function catchOnyx(){
+  onyx.remove();
+  alert('Aww... Onyx broke out... Onyx ran away...');
+}
+
+var golem = document.querySelector('#golem');
+function catchGolem(){
+  golem.remove();
+  alert('Aww... Golem broke out... Golem ran away...');
+}
+
+var beedrill = document.querySelector('#beedrill');
+function catchBeedrill(){
+  beedrill.remove();
+  alert('You Caught Beedrill!');
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var caterpie = document.querySelector('#caterpie');
+function catchCaterpie(){
+  caterpie.remove();
+  alert('You Caught Caterpie!');
+  caught++;
+  numberCaught.innerText = caught;
+}
+
+var scyther = document.querySelector('#scyther');
+function catchScyther(){
+  scyther.remove();
+  alert('Aww... Scyther broke out... Scyther ran away...');
+}
